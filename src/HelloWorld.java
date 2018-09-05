989adae11e0afaaf04e25cec148962e7af07923c
Ryan Parrish <sRyanParrish@yccc.edu>
2018-09-05 19:09:21 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld 
+{
+	
+	// an object is 2 things.
+	// 1. Actions. Methods.
+	// 2. Attributes. Variables.
+	private String name = "Ryan";
+	
+	// Main Method. Starts the program.
+	// Method signature - what makes this method unique. Defines the method.
+	public static void main(String[] args) 
+	{
+		// Prints to the console.
+		System.out.println("Hello World");
+		System.out.println("This is another line");
+	}
+}
