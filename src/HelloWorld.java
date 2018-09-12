de076f4b1fb72b54554b8840e53908b1000a6156
Ryan Parrish <sRyanParrish@yccc.edu>
2018-09-12 18:54:36 -0400

    halfway through class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld 
 {
-	//test commit
+	//test commit 2
 	// an object is 2 things.
 	// 1. Actions. Methods.
 	// 2. Attributes. Variables.
@@ -10,6 +10,12 @@ public class HelloWorld
 	// Main Method. Starts the program.
 	// Method signature - what makes this method unique. Defines the method.
 	public static void main(String[] args) 
+	{
+		HelloWorld hw = new HelloWorld();
+		hw.sayHello();
+	}
+	//this method says hello when called
+	private void sayHello()
 	{
 		// Prints to the console.
 		System.out.println("Hello World");
