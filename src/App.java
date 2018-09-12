ea9179f77dac76ef399ca779058da2c222e10518
Ryan Parrish <sRyanParrish@yccc.edu>
2018-09-12 19:44:33 -0400

    end of class, first Class project: Hangman: consol presents Prompt, Mask of word, and hint.

---
+++ src/App.java
@@ -0,0 +1,35 @@
+//Namespace - Keeping objects unique.
+//Translate to a folder.
+//Helps you organize 'like' code
+//package firstJavaProject
+
+// App is the entry point to our application
+public class App {
+	
+	public static void main(String[] args)
+	{
+		User u1 = new User();
+		u1.setFirstName("Ryan");
+		u1.setLastName("Parrish");
+		u1.setEmail("ryanparrish@yup.com");
+		u1.setUserName("smashnwasted");
+		
+		User u2 = new User();
+		u2.setFirstName("Dave");
+		u2.setLastName("Dangle");
+		u2.setEmail("shouldawouldacoulda@yup.com");
+		u2.setUserName("DingleDangle213");
+		
+		User u3 = new User();
+		u3.setFirstName("Robin");
+		u3.setLastName("Williams");
+		u3.setEmail("L@stlaugh@yup.com");
+		u3.setUserName("HaHaorD13");
+		
+		System.out.println(u1.getUserName());
+		System.out.println(u2.getUserName());
+		System.out.println(u3.getUserName());
+	
+	}
+	
+}
--- src/User.java
+++ src/User.java
@@ -1,4 +1,39 @@
 
-public class User {
+public class User 
+{
+	private String userName;
+	private String email;
+	private String firstName;
+	private String lastName;
+	
+	//Method Signature consists of...
+	//Accessor - public, private or protect.
+	//Return type - Any type or void for nothing
+	//Method name - make it good so people know what the method does
+	//Argument list - parameters passes to the method
 
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastrName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
