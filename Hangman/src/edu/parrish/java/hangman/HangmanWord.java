4638bba5ae2b81270d42074ae5819fbc44ecaace
smashnwasted <43018020+smashnwasted@users.noreply.github.com>
2018-10-03 11:13:02 -0400

    working on it

---
+++ Hangman/src/edu/parrish/java/hangman/HangmanWord.java
@@ -0,0 +1,9 @@
+package edu.parrish.java.hangman;
+
+public class HangmanWord {
+    String Gameword = "Guitar";
+
+    for (int i, i < word.length(Gameword), i++)
+
+        System.out.println("*")
+}
